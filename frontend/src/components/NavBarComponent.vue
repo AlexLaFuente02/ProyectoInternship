7561b652fd49ded26c8253835c9015c83010be53
OsquiMenacho28 <osqui.menacho2002@gmail.com>
2023-10-09 20:03:19 -0400

    Update NavBarComponent.vue

--- frontend/src/components/NavBarComponent.vue
+++ frontend/src/components/NavBarComponent.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="navbar navbar-light">
-    <nav class="navbar navbar-expand-lg bg-body-secondary bg-opacity-75 w-100 shadow">
-      <div class="container-md">
+  <div class="navbar navbar-light p-0">
+    <nav class="navbar navbar-expand-lg bg-body-secondary bg-opacity-75 w-100">
+      <div class="container">
         <span class="navbar-brand fw-light">
           <img class="h-50 w-25 me-5" src="@/components/images/USEI.png" alt="USEI"/>
-          USEI
         </span>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <div class="navbar-nav mb-lg-0">
+          <div class="navbar-nav mr-auto mt-2 mt-lg-0">
             <router-link class="nav-item nav-link" to="/">Home</router-link> |
             <router-link class="nav-item nav-link" to="/login">Login</router-link>
           </div>
