cbf408bdbb0cbd0caf61e0c82b126cf677d4e1a8
OsquiMenacho28 <osqui.menacho2002@gmail.com>
2023-11-01 19:24:57 -0400

    Login & ApplyInternship

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,70 +1,98 @@
 //Librerias
-import { createApp } from 'vue';
-import {createRouter, createWebHistory} from 'vue-router';
-import {createPinia} from 'pinia';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import PrimeVue from 'primevue/config';
-import App from './App.vue';
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { createPinia } from "pinia";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import PrimeVue from "primevue/config";
+import App from "./App.vue";
 //Paginas
-import LoginPage from '@/views/common/LoginPage.vue';
-import PrincipalPage from '@/views/common/PrincipalPage.vue';
-import MoreInformation from '@/views/common/MoreInformation.vue';
-import InstitutionsCommon from '@/views/common/InstitutionsCommon.vue';
-import InternshipsCommon from '@/views/common/InternshipsCommon.vue';
-import UserRegister from '@/views/common/UserRegister.vue';
+import LoginPage from "@/views/common/LoginPage.vue";
+import PrincipalPage from "@/views/common/PrincipalPage.vue";
+import MoreInformation from "@/views/common/MoreInformation.vue";
+import InstitutionsCommon from "@/views/common/InstitutionsCommon.vue";
+import InternshipsCommon from "@/views/common/InternshipsCommon.vue";
+import UserRegister from "@/views/common/UserRegister.vue";
 //Paginas Student
-import StudentRegister from '@/views/student/StudentRegister.vue';
+import StudentRegister from "@/views/student/StudentRegister.vue";
+import ApplyForAnInternship from "@/views/student/ApplyForAnInternship.vue";
 //Paginas Institution
 //Paginas Admin
+
 //Librerias de PrimeVue
-import ButtonPrime from 'primevue/button';
+import ButtonPrime from "primevue/button";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
 //Main
-import './assets/main.css';
+import "./assets/main.css";
 
-//Rutas 
+//Rutas
 const routes = [
-    //Common
-    {name: 'PrincipalPage', path: '/', component: PrincipalPage},
-    {name: 'LoginPage', path: '/login', component: LoginPage},
-    {name: 'StudentRegister', path: '/student/register', component: StudentRegister},
-    {name: 'MoreInformation', path: '/moreInformation', component: MoreInformation},
-    {name: 'InstitutionsCommon', path: '/institutionsCommon', component: InstitutionsCommon},
-    {name: 'InternshipsCommon', path: '/internshipsCommon', component: InternshipsCommon},
-    {name: 'UserRegister', path: '/userRegister', component: UserRegister},
-    //Student
-    {name: 'StudentRegister', path: '/student/register', component: StudentRegister},
-    //Institution
-    
+  //Common
+  { name: "PrincipalPage", path: "/", component: PrincipalPage },
+  { name: "LoginPage", path: "/login", component: LoginPage },
+  {
+    name: "MoreInformation",
+    path: "/moreInformation",
+    component: MoreInformation,
+  },
+  {
+    name: "InstitutionsCommon",
+    path: "/institutionsCommon",
+    component: InstitutionsCommon,
+  },
+  {
+    name: "InternshipsCommon",
+    path: "/internshipsCommon",
+    component: InternshipsCommon,
+  },
+  { name: "UserRegister", path: "/userRegister", component: UserRegister },
+  //Student
+  {
+    name: "StudentRegister",
+    path: "/student/register",
+    component: StudentRegister,
+  },
+  {
+    name: "ApplyForAnInternship",
+    path: "/student/ApplyForAnInternship",
+    component: ApplyForAnInternship,
+  },
+  //Institution
 
-    //Usei-Admin
+  //Usei-Admin
 ];
 // Router
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
+
 //Pinia para el manejo de estados
 const pinia = createPinia();
+
 //Librerias de fontawesome para los iconos
 library.add(fab);
 library.add(fas);
+
 // App
 const app = createApp(App);
+
 //Uso de las librerias
 app.use(router);
 app.use(pinia);
 app.use(PrimeVue);
+
 //Componentes de fontawesome
-app.component('font-awesome-icon', FontAwesomeIcon);
+app.component("font-awesome-icon", FontAwesomeIcon);
+
 //Componentes de PrimeVue
-app.component('ButtonPrime', ButtonPrime);
+app.component("ButtonPrime", ButtonPrime);
+
 //Montar la aplicacion
-app.mount('#app');
+app.mount("#app");
