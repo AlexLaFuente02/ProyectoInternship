24bf7b171191efc6f5ded49240317b7ac1dc8514
jhessika-zarate <jhessikazarate@gmail.com>
2023-11-08 15:08:25 -0400

    arreglo de inicio de empresaaaa

--- Frontend/src/views/institution/InstitutionPrincipalPage.vue
+++ Frontend/src/views/institution/InstitutionPrincipalPage.vue
@@ -1,54 +1,374 @@
 <template>
-    <div class="login-container">
-      <div class="top-section">
+  <main>
+    <div class="container__landingPage">
+      <div class="landing__page__information">
+        <div class="container__header__description">
+          <h1>¡Bienvenido a la plataforma de pasantías!</h1>
+          <p>
+            En esta plataforma podrás encontrar pasantías y prácticas
+            profesionales que te ayudarán a desarrollar tus habilidades y
+            competencias en el mundo laboral.
+          </p>
+          
+        </div>
+        <div class="top-section">
         <div class="left-section">
-          <h2>Bienvenido, desde esta página puedes agregar nuevas convocatorias y revisar las solicitudes de estudiantes que recibiste.</h2>
+          <p>Bienvenido, desde esta página puedes agregar nuevas convocatorias y revisar las solicitudes de estudiantes que recibiste.</p>
         </div>
         <div class="right-section">
           <img src="https://img.freepik.com/vector-premium/cute-dibujos-animados-perro-pug-sentado-fondo-aislado_701683-46.jpg?w=996" alt="Logo de la empresa" class="company-logo" />
         </div>
       </div>
-  
-      <div class="quick-action">
-        <h3>Acción rápida</h3>
-        <div class="action-buttons">
+      <hr/>
+        <div class="container__header__page">
+        <div class="container__header__description">
+          <h1>¡Acción rápida!</h1>
+          <div class="action-buttons">
           <button @click="addNewConvocatoria">Agregar Convocatoria</button>
           <button @click="misConvocatorias">Mis Convocatorias</button>
           <button @click="bandejaSolicitudes">Bandeja de Solicitudes</button>
         </div>
-      </div>
-  
-      <div class="active-convocatorias">
-        <h3>Convocatorias activas o Solicitudes recibidas</h3>
-        <div class="cards">
-          <!-- Lógica para mostrar las convocatorias o solicitudes recibidas -->
+
         </div>
       </div>
+      <hr/>
+
+      <div class="container__problem">
+        <div class="container__problem__image">
+          <img src="https://s.yimg.com/ny/api/res/1.2/0h_DQCqc7Q8A.rc5jVk2kA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNw--/https://media.zenfs.com/en/homerun/feed_manager_auto_publish_494/cc75c92508ef9b1713bfca5cc2f1a3d3" />
+        </div>
+        <div class="container__problem__description">
+          <h1>¿Cuál es el problema?</h1>
+          <p>
+            En la actualidad, los estudiantes y graduados no cuentan con una
+            plataforma que les permita encontrar pasantías y prácticas
+            profesionales de manera fácil y rápida.
+          </p>
+        </div>
+      </div>
+      <hr/>
+      <div class="container__header__page">
+        <div class="container__header__description">
+          <h1>¡Convocatorias activas o Solicitudes recibidas!</h1>
+          <p>
+            En esta plataforma podrás encontrar pasantías y prácticas
+            profesionales que te ayudarán a desarrollar tus habilidades y
+            competencias en el mundo laboral.
+          </p>
+          <Button 
+              text="Crear Cuenta" 
+              :color="0" 
+              :disabled="false"
+              @option-selected="createAccount"
+              >
+            </Button>
+
+        </div>
+        <div class="container__carrousel">
+          <Carousel :listImages="PhotoService" />
+        </div>
+      </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      addNewConvocatoria() {
-        // Lógica para el botón 'Agregar Convocatoria'
-        console.log('Agregar Convocatoria');
-      },
-      misConvocatorias() {
-        // Lógica para el botón 'Mis Convocatorias'
-        console.log('Mis Convocatorias');
-      },
-      bandejaSolicitudes() {
-        // Lógica para el botón 'Bandeja de Solicitudes'
-        console.log('Bandeja de Solicitudes');
+  </main>
+</template>
+<script>
+import Carousel from "@/components/common/Carousel.vue";
+import Button from "@/components/common/Button.vue";
+export default {
+  components: {
+    Carousel,
+    Button
+  },
+  data() {
+    return {
+      PhotoService:[
+      {
+            itemImageSrc: 'https://www.animafestexperience.net/internshipsabroad/wp-content/uploads/2019/08/ANIMAFEST-PASANTIAS-REMUNERADAS-min-min-800x333.jpg',
+            thumbnailImageSrc: 'https://www.animafestexperience.net/internshipsabroad/wp-content/uploads/2019/08/ANIMAFEST-PASANTIAS-REMUNERADAS-min-min-800x333.jpg',
+            alt: 'Explota todas tus habilidades ¡Enhorabuena!',
+            title: '¡Bienvenido al Mundo Laboral!'
+          },
+          {
+            itemImageSrc: 'https://idiomasseif.com/wp-content/uploads/2019/04/importancia-del-ingles-en-el-mundo-laboral.jpg',
+            thumbnailImageSrc: 'https://idiomasseif.com/wp-content/uploads/2019/04/importancia-del-ingles-en-el-mundo-laboral.jpg',
+            alt: 'Renueva tu empresa con los mejores estudiantes del país.',
+            title: '¡Como institución puedes contratar a estudiantes y graduados!'
+          },
+          {
+            itemImageSrc: 'https://media.gq.com.mx/photos/642447f1c632255eae24a045/master/w_1920,c_limit/trabajo.jpg',
+            thumbnailImageSrc: 'https://media.gq.com.mx/photos/642447f1c632255eae24a045/master/w_1920,c_limit/trabajo.jpg',
+            alt: '¡Estamos seguros de que pronto encontraras tu lugar...!',
+            title: '¡Muéstranos todo lo que sabes!'
+          },
+      ],
+    };
+  },
+  methods: {
+    createAccount(option) {
+      if (option) {
+        console.log("createAccount");
+        this.$router.push("/UserRegister");
       }
-    }
-  };
-  </script>
+    },
+  },
   
-  <style scoped>
-  /* Estilos para la página de inicio de sesión */
-  @media (max-width: 480px) {
+};
+</script>
+<style scoped>
+.container__landingPage {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  
+}
+.landing__page__information{
+  background-color: #FDFEFF;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  box-shadow: 0 24px 64px -2px rgba(0, 0, 0, 0.02),
+    0 6px 16px -2px rgba(0, 0, 0, 0.06), 0 2px 6px -2px rgba(0, 0, 0, 0.08);
+}
+.dark-theme .landing__page__information {
+  background-color: #434b54;
+}
+.container__header__page{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+}
+.container__header__description{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3% 7%;
+}
+.container__header__description h1{
+  font-size: 1.5rem;
+  text-align: center;
+}
+.container__header__description p{
+  font-size: 1rem;
+  text-align: center;
+}
+.container__header__description Button{
+  margin-top: 25%;
+}
+.container__carrousel {
+  display: flex;
+  padding: 1rem;
+  align-items: center;
+  justify-content: center;
+  width: 600px;
+  height: 400px;
+  z-index: 0;
+}
+.container__problem{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  margin-top: 2%;
+}
+.container__problem__description{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3% 7%;
+}
+.container__problem__description h1{
+  font-size: 1.5rem;
+  text-align: center;
+}
+.container__problem__description p{
+  font-size: 1rem;
+  text-align: center;
+}
+.container__problem__image{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3% 7%;
+}
+.container__problem__image img{
+  width: 400px;
+  height: 300px;
+}
+.container__solution{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  margin-top: 2%;
+}
+.container__solution__description{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3% 7%;
+}
+.container__solution__description h1{
+  font-size: 1.5rem;
+  text-align: center;
+}
+.container__solution__description p{
+  font-size: 1rem;
+  text-align: center;
+}
+hr{
+  width: 90%;
+  margin-top: 2%;
+  margin-bottom: 2%;
+  border: 1px solid #E4E5E5;
+  /*Centrar hr*/
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+.dark-theme hr{
+  border: 1px solid #5B646D;
+}
+@media screen and (max-width: 1250px) {
+  .container__header__page{
+    flex-direction: column;
+  }
+  .container__header__description{
+    padding: 3% 7%;
+  }
+  .container__carrousel {
+    width: 400px;
+    height: 300px;
+  }
+  .container__problem{
+    flex-direction: column-reverse;
+  }
+  .container__problem__description{
+    padding: 3% 7%;
+  }
+  .container__problem__image{
+    padding: 3% 7%;
+  }
+  .container__problem__image img{
+    width: 300px;
+    height: 200px;
+  }
+  .container__solution{
+    flex-direction: column;
+  }
+  .container__solution__description{
+    padding: 3% 7%;
+  }
+}
+@media screen and (max-width: 768px) {
+  .container__header__page{
+    flex-direction: column;
+  }
+  .container__header__description{
+    padding: 3% 7%;
+  }
+  .container__carrousel {
+    width: 300px;
+    height: 200px;
+  }
+  .container__problem{
+    flex-direction: column-reverse;
+  }
+  .container__problem__description{
+    padding: 3% 7%;
+  }
+  .container__problem__image{
+    padding: 3% 7%;
+  }
+  .container__problem__image img{
+    width: 200px;
+    height: 150px;
+  }
+  .container__solution{
+    flex-direction: column;
+  }
+  .container__solution__description{
+    padding: 3% 7%;
+  }
+}
+@media screen and (max-width: 425px) {
+  .container__header__page{
+    flex-direction: column;
+  }
+  .container__header__description{
+    padding: 3% 7%;
+  }
+  .container__carrousel {
+    width: 200px;
+    height: 150px;
+  }
+  .container__problem{
+    flex-direction: column-reverse;
+  }
+  .container__problem__description{
+    padding: 3% 7%;
+  }
+  .container__problem__image{
+    padding: 3% 7%;
+  }
+  .container__problem__image img{
+    width: 150px;
+    height: 100px;
+  }
+  .container__solution{
+    flex-direction: column;
+  }
+  .container__solution__description{
+    padding: 3% 7%;
+  }
+}
+@media screen and (max-width: 375px) {
+  .container__header__page{
+    flex-direction: column;
+  }
+  .container__header__description{
+    padding: 3% 7%;
+  }
+  .container__carrousel {
+    width: 200px;
+    height: 150px;
+  }
+  .container__problem{
+    flex-direction: column-reverse;
+  }
+  .container__problem__description{
+    padding: 3% 7%;
+  }
+  .container__problem__image{
+    padding: 3% 7%;
+  }
+  .container__problem__image img{
+    width: 150px;
+    height: 100px;
+  }
+  .container__solution{
+    flex-direction: column;
+  }
+  .container__solution__description{
+    padding: 3% 7%;
+  }
+}
+/**ARREGLO0S   DE COSAS */
+
+/* Estilos para la página de inicio de sesión */
+@media (max-width: 480px) {
   .top-section {
     flex-direction: column;
     align-items: center;
@@ -83,11 +403,13 @@
   
   .left-section {
     width: 50%;
+    margin: 5%;
   }
   
   .right-section {
     width: 50%;
     text-align: right;
+    margin: 5%;
   }
   
   h2 {
@@ -116,7 +438,7 @@
     display: flex;
     justify-content: space-evenly;
     flex-wrap: wrap;
-    margin-top: 10px;
+    margin-top: 0%;
   }
   
   button {
@@ -127,18 +449,15 @@
     border-radius: 5px;
     cursor: pointer;
     font-size: 16px;
-    margin: 0 5px 10px 5px;
+    margin: 0% 5px 10px 5px;
   }
   
-  .active-convocatorias {
-    margin-top: 30px;
+  
+  @media (min-width: 700px) {
+    .company-logo {
+    max-width:auto;
+    height: 280px;
   }
   
-  .cards {
-    /* Estilos para mostrar las convocatorias o solicitudes recibidas */
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-  }
-  </style>
-  
+}
+</style>
