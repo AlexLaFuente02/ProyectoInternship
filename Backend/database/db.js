76ec3254376f542c28c7240e8b06570f309cac16
jhessika-zarate <jhessikazarate@gmail.com>
2023-11-26 17:56:12 -0400

    Update db.js

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -4,7 +4,7 @@ const sequelize = new Sequelize({
   dialect: "mysql", // El dialecto de la base de datos que estás utilizando
   host: "localhost",
   username: "root",
-  password: "admin",
+  password: "admi",
   database: "internshipdb", // Cambia esto al nombre de tu base de datos
   port: 3306, // Cambia esto al puerto de tu base de datos si es diferente
   timezone: "-04:00", // Esto establece la zona horaria para todas las operaciones de Sequelize
