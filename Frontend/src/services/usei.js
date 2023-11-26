03ce05df3faf64cb4371682ae42fa0ba9da3a678
AlexLaFuente02 <104600200+AlexLaFuente02@users.noreply.github.com>
2023-11-26 17:44:43 -0400

    POST Institucion Funcional

--- Frontend/src/services/usei.js
+++ Frontend/src/services/usei.js
@@ -26,6 +26,6 @@ export const activateInstitution = async (institutionId) => {
 export const loadUseiInternship = async () => {
     /*Cambiar por la ruta de la api que corresponda*/
     //Se esta usando la ruta de la api de prueba
-    const response = await axios.get(`${rutaUsei}/convocatoria`);
+    const response = await axios.get(`${rutaUsei}/convocatoria/activas`);
     return response.data;
 }
