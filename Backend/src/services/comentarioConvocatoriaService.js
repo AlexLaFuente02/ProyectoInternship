0396a32354cc1a50f94cbf97c75ecfd08c720ac9
AlexLaFuente02 <104600200+AlexLaFuente02@users.noreply.github.com>
2023-12-03 22:53:36 -0400

    CRUD ComentarioConvocatoriaYPostulante

---
+++ Backend/src/services/comentarioConvocatoriaService.js
@@ -0,0 +1,246 @@
+const ComentarioConvocatoriaENT = require("../ENT/ComentarioConvocatoriaENT");
+const PostulacionENT = require("../ENT/PostulacionENT");
+const ConvocatoriaENT = require("../ENT/ConvocatoriaENT");
+const ResponseDTO = require("../DTO/ResponseDTO");
+const EstudianteENT = require("../ENT/EstudianteENT");
+const EstudianteDTO = require("../DTO/EstudianteDTO");
+const EstadoPostulacionENT = require("../ENT/EstadoPostulacionENT");
+const EstadoConvocatoriaENT = require("../ENT/EstadoConvocatoriaENT");
+const InstitucionENT = require("../ENT/InstitucionENT");
+const TiempoAcumplirENT = require("../ENT/TiempoACumplirENT");
+const ComentarioConvocatoriaDTO = require("../DTO/ComentarioConvocatoriaDTO");
+
+const getAllComentarios = async () => {
+    try {
+        const comentarios = await ComentarioConvocatoriaENT.findAll({
+            include: [
+                {
+                    model: PostulacionENT,
+                    as: 'postulacion',
+                    include: [
+                        {
+                            model: EstudianteENT,
+                            as: 'estudiante'
+                        },
+                        { model: ConvocatoriaENT, as: 'convocatoria' },
+                        { model: EstadoPostulacionENT, as: 'estadopostulacion' }
+                    ]
+                },
+                { 
+                    model: ConvocatoriaENT, 
+                    as: 'convocatoria',
+                    include: [
+                        { model: EstadoConvocatoriaENT, as: 'estadoconvocatoria' },
+                        { model: InstitucionENT, as: 'institucion'},
+                        { model: TiempoAcumplirENT, as: 'tiempoacumplir' }
+                    ]
+
+                }
+            ]
+        });
+
+        const comentariosDTO = comentarios.map(comentario => (
+            new ComentarioConvocatoriaDTO(
+                comentario.id,
+                comentario.comentario,
+                comentario.puntuacion,
+                {
+                    id: comentario.postulacion.id,
+                    fechapostulacion: comentario.postulacion.fechapostulacion,
+                    estadopostulacion: comentario.postulacion.estadopostulacion,
+                    estudiante: new EstudianteDTO(
+                        comentario.postulacion.estudiante.id,
+                        comentario.postulacion.estudiante.usuario_id,
+                        comentario.postulacion.estudiante.nombres,
+                        comentario.postulacion.estudiante.apellidos,
+                        comentario.postulacion.estudiante.carnetidentidad,
+                        comentario.postulacion.estudiante.correoelectronico,
+                        comentario.postulacion.estudiante.celularcontacto,
+                        comentario.postulacion.estudiante.graduado,
+                        comentario.postulacion.estudiante.carrera_id,
+                        comentario.postulacion.estudiante.semestre_id,
+                        comentario.postulacion.estudiante.sede_id,
+                        comentario.postulacion.estudiante.aniograduacion,
+                        comentario.postulacion.estudiante.linkcurriculumvitae
+                    ),
+                    convocatoria: comentario.postulacion.convocatoria
+                },
+                {
+                    id: comentario.convocatoria.id,
+                    areapasantia: comentario.convocatoria.areapasantia,
+                    descripcionfunciones: comentario.convocatoria.descripcionfunciones,
+                    requisitoscompetencias: comentario.convocatoria.requisitoscompetencias,
+                    horario_inicio: comentario.convocatoria.horario_inicio,
+                    horario_fin: comentario.convocatoria.horario_fin,
+                    fecha_solicitud: comentario.convocatoria.fecha_solicitud,
+                    fechaslseccionpasante: comentario.convocatoria.fechaslseccionpasante,
+                    estadoconvocatoria: comentario.convocatoria.estadoconvocatoria,
+                    institucion: comentario.convocatoria.institucion,
+                    tiempoacumplir: comentario.convocatoria.tiempoacumplir
+                }
+            )
+        ));
+
+        return new ResponseDTO('C-0000', comentariosDTO, 'Comentarios obtenidos correctamente');
+    } catch (error) {
+        return new ResponseDTO('C-1001', null, `Error al obtener todos los comentarios: ${error}`);
+    }
+};
+
+const getComentarioById = async (id) => {
+    try {
+        const comentario = await ComentarioConvocatoriaENT.findByPk(id, {
+            include: [
+                {
+                    model: PostulacionENT,
+                    as: 'postulacion',
+                    include: [
+                        {
+                            model: EstudianteENT,
+                            as: 'estudiante'
+                        },
+                        { model: ConvocatoriaENT, as: 'convocatoria' },
+                        { model: EstadoPostulacionENT, as: 'estadopostulacion' }
+                    ]
+                },
+                { 
+                    model: ConvocatoriaENT, 
+                    as: 'convocatoria',
+                    include: [
+                        { model: EstadoConvocatoriaENT, as: 'estadoconvocatoria' },
+                        { model: InstitucionENT, as: 'institucion'},
+                        { model: TiempoAcumplirENT, as: 'tiempoacumplir' }
+                    ]
+                }
+            ]
+        });
+        
+        if (!comentario) {
+            return new ResponseDTO('C-1002', null, 'Comentario no encontrado');
+        }
+
+        const postulacionDTO = {
+            id: comentario.postulacion.id,
+            fechapostulacion: comentario.postulacion.fechapostulacion,
+            estadopostulacion: comentario.postulacion.estadopostulacion,
+            estudiante: new EstudianteDTO(
+                comentario.postulacion.estudiante.id,
+                comentario.postulacion.estudiante.usuario_id,
+                comentario.postulacion.estudiante.nombres,
+                comentario.postulacion.estudiante.apellidos,
+                comentario.postulacion.estudiante.carnetidentidad,
+                comentario.postulacion.estudiante.correoelectronico,
+                comentario.postulacion.estudiante.celularcontacto,
+                comentario.postulacion.estudiante.graduado,
+                comentario.postulacion.estudiante.carrera_id,
+                comentario.postulacion.estudiante.semestre_id,
+                comentario.postulacion.estudiante.sede_id,
+                comentario.postulacion.estudiante.aniograduacion,
+                comentario.postulacion.estudiante.linkcurriculumvitae
+            ),
+            convocatoria: {
+                id: comentario.postulacion.convocatoria.id,
+                areapasantia: comentario.postulacion.convocatoria.areapasantia,
+                descripcionfunciones: comentario.postulacion.convocatoria.descripcionfunciones,
+                requisitoscompetencias: comentario.postulacion.convocatoria.requisitoscompetencias,
+                horario_inicio: comentario.postulacion.convocatoria.horario_inicio,
+                horario_fin: comentario.postulacion.convocatoria.horario_fin,
+                fecha_solicitud: comentario.postulacion.convocatoria.fecha_solicitud,
+                fechaslseccionpasante: comentario.postulacion.convocatoria.fechaslseccionpasante,
+                estadoconvocatoria: comentario.postulacion.convocatoria.estadoconvocatoria,
+                institucion: comentario.postulacion.convocatoria.institucion,
+                tiempoacumplir: comentario.postulacion.convocatoria.tiempoacumplir
+            }
+        };
+
+        const convocatoriaDTO = {
+            id: comentario.convocatoria.id,
+            areapasantia: comentario.convocatoria.areapasantia,
+            descripcionfunciones: comentario.convocatoria.descripcionfunciones,
+            requisitoscompetencias: comentario.convocatoria.requisitoscompetencias,
+            horario_inicio: comentario.convocatoria.horario_inicio,
+            horario_fin: comentario.convocatoria.horario_fin,
+            fecha_solicitud: comentario.convocatoria.fecha_solicitud,
+            fechaslseccionpasante: comentario.convocatoria.fechaslseccionpasante,
+            estadoconvocatoria: comentario.convocatoria.estadoconvocatoria,
+            institucion: comentario.convocatoria.institucion,
+            tiempoacumplir: comentario.convocatoria.tiempoacumplir
+        };
+
+        return new ResponseDTO('C-0000', {
+            id: comentario.id,
+            comentario: comentario.comentario,
+            puntuacion: comentario.puntuacion,
+            postulacion: postulacionDTO,
+            convocatoria: convocatoriaDTO
+        }, 'Comentario obtenido correctamente');
+    } catch (error) {
+        return new ResponseDTO('C-1002', null, `Error al obtener el comentario: ${error}`);
+    }
+};
+
+
+const createComentario = async (data) => {
+    try {
+        const comentario = await ComentarioConvocatoriaENT.create({
+            comentario: data.comentario,
+            puntuacion: data.puntuacion,
+            postulacion_id: data.postulacion.id,
+            convocatoria_id: data.convocatoria.id
+        });
+        return new ResponseDTO('C-0000', {
+            id: comentario.id,
+            comentario: comentario.comentario,
+            puntuacion: comentario.puntuacion,
+            postulacion: comentario.postulacion,
+            convocatoria: comentario.convocatoria
+        }, 'Comentario creado correctamente');
+    } catch (error) {
+        return new ResponseDTO('C-1003', null, `Error al crear el comentario: ${error}`);
+    }
+};
+
+const updateComentario = async (id, data) => {
+    try {
+        const comentario = await ComentarioConvocatoriaENT.findByPk(id);
+        if (!comentario) {
+            return new ResponseDTO('C-1004', null, 'Comentario no encontrado');
+        }
+        await comentario.update({
+            comentario: data.comentario,
+            puntuacion: data.puntuacion,
+            postulacion_id: data.postulacion.id,
+            convocatoria_id: data.convocatoria.id
+        });
+        return new ResponseDTO('C-0000', {
+            id: comentario.id,
+            comentario: comentario.comentario,
+            puntuacion: comentario.puntuacion,
+            postulacion: comentario.postulacion,
+            convocatoria: comentario.convocatoria
+        }, 'Comentario actualizado correctamente');
+    } catch (error) {
+        return new ResponseDTO('C-1004', null, `Error al actualizar el comentario: ${error}`);
+    }
+};
+
+const deleteComentario = async (id) => {
+    try {
+        const comentario = await ComentarioConvocatoriaENT.findByPk(id);
+        if (!comentario) {
+            return new ResponseDTO('C-1005', null, 'Comentario no encontrado');
+        }
+        await comentario.destroy();
+        return new ResponseDTO('C-0000', comentario, 'Comentario eliminado correctamente');
+    } catch (error) {
+        return new ResponseDTO('C-1005', null, `Error al eliminar el comentario: ${error}`);
+    }
+};
+
+module.exports = {
+    getAllComentarios,
+    getComentarioById,
+    createComentario,
+    updateComentario,
+    deleteComentario
+};
